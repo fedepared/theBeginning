c5242f34fe4a1a83d1a109e3ac027723499ebef6
fede <federico.pared@gmail.com>
2019-09-12 15:07:49 -0300

    Revisar contenedorAbout2, porqué se visualiza junto con el otro contenedor

--- js/javascript.js
+++ js/javascript.js
@@ -35,8 +35,29 @@ $(document).ready(function() {
             
         }); 
 
+        $('.contenedorAbout2').each( function(i){
+            
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+                
+                $(this).animate({'opacity':'0.5'},1500);
+                    
+            }
+            
+        });
         
     
     });
     
 });
+
+
+var contenedor = document.getElementById("contenedor");
+
+function mostrar(){
+    console.log("llegue");
+    contenedor.style.display = 'block';
+}
